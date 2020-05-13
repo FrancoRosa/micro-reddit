6cc9bb8a957951519f3742c22eb0799307c847f2
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-05-13 17:12:45 +0000

    Add post model to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+  has_many :posts
+
   EMAIL_VALIDATOR = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :username, :email, :password, presence: true
   validates :username, :email, uniqueness: true
   validates :username, length:  { in: 4..12 }
   validates :password, length:  { in: 6..16 }
-  validates :email, format: { with: EMAIL_VALIDATOR,
-                              message: "this is not a valid email" }
+  validates :email, 
+    format: { 
+      with: EMAIL_VALIDATOR,
+      message: "this is not a valid email" 
+    }
+
 end
