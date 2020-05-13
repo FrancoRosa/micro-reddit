a956930a502313bc74bd0b1a84f757bedb1f5260
FrancoRosa <km115.franco@gmail.com>
2020-05-13 11:33:59 -0500

    Add User model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  EMAIL_VALIDATOR = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
+  validates :username, length:  { in: 4..12 }
+  validates :password, length:  { in: 6..16 }
+  validates :email, format: { with: EMAIL_VALIDATOR,
+                              message: "this is not a valid email" }
+end
